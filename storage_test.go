adca22eba467457b2aa336edcd4308cb4156bbca
Sotirios Mantziaris <s.mantziaris@live.com>
2016-02-13 01:48:54 +0200

    increasing coverage

--- storage_test.go
+++ storage_test.go
@@ -53,3 +53,64 @@ func TestStorageClose(t *testing.T) {
 		t.Fatalf("Expectations not met, %s", err)
 	}
 }
+
+func TestStorageWrongDbType(t *testing.T) {
+
+	dbInner, _, _ := sqlmock.New()
+
+	_, err := NewStorageFinalized(dbInner, 3, "Event")
+
+	if err == nil {
+        t.Fatal("Should have been returning a error")
+    }
+}
+
+func TestNewStorageWrongDbType(t *testing.T) {
+
+	_, err := NewStorage(3, "123" ,"Event")
+
+	if err == nil {
+        t.Fatal("Should have been returning a error")
+    }
+}
+
+func TestNewStorageOpenError(t *testing.T) {
+
+	_, err := NewStorage(MSSQL, "123" ,"Event")
+
+	if err == nil {
+        t.Fatal("Should have been returning a error")
+    }
+}
+
+var convertToTypeTests = []struct {
+    in  string
+    out DbType
+    hasError bool
+}{
+    {"mssql", MSSQL, false},
+    {"MSSQL", MSSQL, false},
+    {"MsSQL", MSSQL, false},
+    {"MsSql", MSSQL, false},
+    {"postgresql", PostgreSQL, false},
+    {"PostgreSQL", PostgreSQL, false},
+    {"POSTGRESQL", PostgreSQL, false},
+    {"xxx", PostgreSQL, true},
+}
+
+func TestConvertToType(t *testing.T)  {
+    
+    for _, test := range convertToTypeTests {
+        
+        actual, err := ConvertToDbType(test.in)
+        
+        if err != nil && test.hasError {
+            
+            if(!test.hasError){
+                t.Errorf("Expected no errors but shit happens for input %s", test.in)    
+            }            
+        } else if actual != test.out {
+            t.Errorf("Expected %d got %d for input %s", test.out, actual, test.in)
+        }
+    }
+}
